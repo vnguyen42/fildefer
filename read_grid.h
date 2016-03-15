3ae134290cad2d0d3b1cfd2046b63ae80135d185
Vincent NGUYEN <vnguyen@e1r3p18.42.fr>
2016-03-15 16:31:39 +0100

    norme all

--- read_grid.h
+++ read_grid.h
@@ -6,23 +6,23 @@
 /*   By: vnguyen <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/17 19:42:13 by vnguyen           #+#    #+#             */
-/*   Updated: 2016/03/14 18:56:37 by vnguyen          ###   ########.fr       */
+/*   Updated: 2016/03/15 16:31:22 by vnguyen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# ifndef READ_GRID_H
-#define READ_GRID_H
+#ifndef READ_GRID_H
+# define READ_GRID_H
 # include <mlx.h>
 # include <fcntl.h>
 
-typedef struct s_point
+typedef struct	s_point
 {
 	int x;
 	int y;
 	int z;
 }				t_point;
 
-int		**read_grid(char *filepath);
-t_point	get_file_dimensions(char *filepath);
+int				**read_grid(char *filepath);
+t_point			get_file_dimensions(char *filepath);
 
 #endif
